2b80c61598687ec1dcfaa6f1babfef50019e7a9b
David <87726263@qq.com>
2017-05-09 16:59:46 +0800

    修改测试用例

--- index.js
+++ index.js
@@ -192,20 +192,16 @@ function book(app){
         })
     })
 
-    function testf(res,url){
-        class a{
-            get(){
-                return "1"
-            }
+    async function testf(res,url){
+        try{
+            const origin = require('./originlib/BIQUGE')
+            var biquge = new origin()
+            var book = await biquge.getBook("大主宰")
+            res.send(JSON.stringify(book))
+        }catch(e){
+            res.send(JSON.stringify(e))
         }
-        var b = new a()
-        console.log(b.get())
-        res.send(b.get())
-        return 
-        // const origin = require('./originlib/BIQUGE')
-        // var biquge = new origin()
-        // var book = await biquge.getBook("大主宰")
-        // res.send(JSON.stringify(book))
+        
         // var data =  await request.get(url)
         // res.send(data)
     }
