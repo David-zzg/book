bd6f373e183ad48e7d5b4b173125ae19e6f6697d
David <87726263@qq.com>
2017-05-12 15:12:54 +0800

    移除测试环境的api

--- book-client/src/index.js
+++ book-client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 window.HOST = "http://localhost:3000"
-window.API = "http://localhost:4000"
+window.API = ""
 ReactDOM.render(
   <App />,
   document.getElementById('root')
