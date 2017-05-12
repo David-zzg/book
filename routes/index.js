acb40082d61556b233d2287feba9b48ce2ef0ba1
David <87726263@qq.com>
2017-05-12 16:07:55 +0800

    路由项梗概

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,17 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.get('/menu/:origin/:book', function(req, res, next) {
   // console.log('touch')
-  res.sendFile(path.resolve(__dirname,"../public/index2.html"))
+  res.sendFile(path.resolve(__dirname,"../public/index.html"))
   // res.render('index', { title: 'Express' });
 });
 router.get('/detail/:origin/:book', function(req, res, next) {
   // console.log('touch')
-  res.sendFile(path.resolve(__dirname,"../public/index2.html"))
+  res.sendFile(path.resolve(__dirname,"../public/index.html"))
   // res.render('index', { title: 'Express' });
 });
 router.get('/search/:origin/:book', function(req, res, next) {
   // console.log('touch')
-  res.sendFile(path.resolve(__dirname,"../public/index2.html"))
+  res.sendFile(path.resolve(__dirname,"../public/index.html"))
   // res.render('index', { title: 'Express' });
 });
 
